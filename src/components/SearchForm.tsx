79a710e4817a88fdac7a2214925bf70f5041a25a
visvoker <s2089533@gmail.com>
2025-04-17 19:38:21 +0800

    search component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    fetch();
+  }, [searchText]);
+
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
         spellCheck="false"
         type="text"
         required
