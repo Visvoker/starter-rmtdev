49e014b5d5226c2eac6ad9265e133e6544b63b61
visvoker <s2089533@gmail.com>
2025-04-17 15:51:13 +0800

    Compose the app with components

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,5 +1,11 @@
+import JobListItem from "./JobListItem";
+
 export function JobList() {
-  return <ul className="job-list"></ul>;
+  return (
+    <ul className="job-list">
+      {/* <JobListItem /> */}
+    </ul>
+  );
 }
 
 export default JobList;
