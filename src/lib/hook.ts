565e66e3d923709c70ae513f3a4ee1dffa7dbf7c
visvoker <s2089533@gmail.com>
2025-04-21 18:44:02 +0800

    UseDebounce TypeScript(Generics)

--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -67,14 +67,14 @@ export function useJobItems(searchText: string) {
   return { jobItemsSliced, isLoading, totalNumberOfResult } as const;
 }
 
-export function useDebounce(value, delay: number) {
+export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     const timerId = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => clearTimeout(timerId);
-  }, [value]);
+  }, [value, delay]);
 
   return debouncedValue;
 }
