35023e0338f669c66775a6596455bc8d21487738
visvoker <s2089533@gmail.com>
2025-04-22 16:53:50 +0800

    Type return value of fetcher function/Loading State

--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
-import { JobItem } from "./type";
+import { JobItem, JobItemExpanded } from "./type";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
 
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: JobItemExpanded;
+};
+
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+  const response = await fetch(`${BASE_API_URL}/${id}`);
+  const data = await response.json();
+  return data;
+};
+
 export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery({
+  const { data, isInitialLoading } = useQuery({
     queryKey: ["job-item", id],
-    queryFn: async () => {
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      return data;
+    queryFn: () => {
+      return id ? fetchJobItem(id) : null;
     },
     staleTime: 1000 * 60 * 60,
     refetchOnWindowFocus: false,
@@ -18,8 +27,7 @@ export function useJobItem(id: number | null) {
     onError: () => {},
   });
 
-  const jobItem = data?.jobItem;
-  return { jobItem, isLoading } as const;
+  return { jobItem: data?.jobItem, isLoading: isInitialLoading } as const;
 }
 
 export function useJobItems(searchText: string) {
