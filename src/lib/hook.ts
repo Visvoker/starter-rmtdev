d33f7ccf0f28343b490420fbd581a805d66f3774
visvoker <s2089533@gmail.com>
2025-04-26 19:39:42 +0800

    Error Handling when fetch data

--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -10,6 +10,11 @@ type JobItemApiResponse = {
 
 const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
   const response = await fetch(`${BASE_API_URL}/${id}`);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
   const data = await response.json();
   return data;
 };
@@ -24,7 +29,9 @@ export function useJobItem(id: number | null) {
     refetchOnWindowFocus: false,
     retry: false,
     enabled: Boolean(id),
-    onError: () => {},
+    onError: (error) => {
+      console.log(error);
+    },
   });
 
   return { jobItem: data?.jobItem, isLoading: isInitialLoading } as const;
