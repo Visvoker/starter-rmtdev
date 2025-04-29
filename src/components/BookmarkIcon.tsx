5208d4724f34562f2459f87def4bb3231e8df564
visvoker <s2089533@gmail.com>
2025-04-29 16:08:58 +0800

    Keep track of bookmarked Ids in context API

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,20 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useContext } from "react";
+import { BookmarksContext } from "../context/BookmarksContextProvider";
 
-export default function BookmarkIcon() {
+type BookmarkIconProps = {
+  id: number;
+};
+
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
+
   return (
     <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+      <BookmarkFilledIcon
+        onClick={() => handleToggleBookmark(id)}
+        className={`${bookmarkedIds.includes(id) ? "filled" : ""}`}
+      />
     </button>
   );
 }
---
+++ src/context/BookmarksContextProvider.tsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+export const BookmarksContext = createContext(null);
+
+type BookmarksContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export default function BookmarksContextProvider({
+  children,
+}: BookmarksContextProviderProps) {
+  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+  console.log(bookmarkedIds);
+
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkedIds.includes(id)) {
+      setBookmarkedIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setBookmarkedIds((prev) => [...prev, id]);
+    }
+  };
+
+  return (
+    <BookmarksContext.Provider
+      value={{
+        bookmarkedIds,
+        handleToggleBookmark,
+      }}
+    >
+      {children}
+    </BookmarksContext.Provider>
+  );
+}
